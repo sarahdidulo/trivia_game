4e7037f03ed596362bf086d3eb7e1bb7660bc6ff
s-didulo <s.didulo@creativehope.co.jp>
2022-03-23 11:37:02 +0700

    applied changes in show answer

--- js/script.js
+++ js/script.js
@@ -6,33 +6,31 @@ $(window).ready(function(){
     let showAnswer = false;
     let time = 0;
     let closeModal;
+    let cardId;
+    let ansToo;
 
     $(".card-number").click(function(){
+        ansToo = false;
         closeModal = false;
         showAnswer = false; 
-        time = 60;     
-        let cardId = $(this).attr("id");
+        time = 2;     
+        cardId = $(this).attr("id");
         currentModal = "#"+cardId+"-m";
         if(!$(this).hasClass("cardClicked")){   
             $(currentModal).css("display", "flex");          
-            timeVal = timer(time);
-            $(".btn-close").click(function(){  
-                closeModal = true;    
-                currentPoints = parseInt(currentModal.slice(6,9));
-                $(".card-modal-wrapper").css("display", "none");
-                $("#"+cardId).addClass("cardClicked");
-            }); 
+            timer(time);            
         }
     });  
 
     function timer(time){
         let timer = setInterval(function(){
-            console.log(closeModal);
             if(time == 0 && showAnswer == false && closeModal == false){   
                 $(currentModal+' .seconds').text(time+"s");             
                 $(currentModal+" .question").css("display", "none");
                 $(currentModal+" .timesUp").css("display", "block");
-                $(currentModal+" .btn-steal").removeAttr("disabled");   
+                if(ansToo == false){
+                    $(currentModal+" .btn-steal").removeAttr("disabled");  
+                }                
                 clearInterval(timer);        
             } else if(showAnswer == true && closeModal == false){
                 clearInterval(timer);
@@ -46,16 +44,27 @@ $(window).ready(function(){
         }, 1000);
     }
 
-    $(".btn-showAnswer").click(function(){         
+    $(currentModal+" .btn-close").click(function(){  
+        closeModal = true;    
+        currentPoints = parseInt(currentModal.slice(6,9));
+        $(currentModal).css("display", "none");
+        $("#"+cardId).addClass("cardClicked");
+    }); 
+
+    $(currentModal+" .btn-showAnswer").click(function(){         
         $(currentModal+" .question").css("display", "none");
         $(currentModal+" .timesUp").css("display", "none");
         $(currentModal+" .card-answer").css("display", "block");
         showAnswer = true;
     }); 
 
-    $(".btn-steal").click(function(){
+    $(currentModal+ " .btn-steal").click(function(){
+        ansToo = true;
         $(currentModal+" .question").css("display", "block");
         $(currentModal+" .timesUp").css("display", "none");
+        $(currentModal+" .btn-steal").prop("disabled", true); 
+        time = 5;
+        timer(time);
     });
 
     
